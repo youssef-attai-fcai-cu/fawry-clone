3c0c7673699e78241a8b49bfaf1eb1943a57c0a9
sherif3hassan <102480451+sherif3hassan@users.noreply.github.com>
2022-12-04 00:31:37 +0200

    Update Refund request controller
    ADD transaction repository, userID , Create refund request method and get user transaction method

--- src/Refund/RefundRequestController.java
+++ src/Refund/RefundRequestController.java
@@ -1,9 +1,23 @@
 package Refund;
 
+import java.util.List;
+
 public class RefundRequestController {
     private final RefundRepository refundRepository;
+    private final TransactionRepository transactionRepository;
+    private final int userID;
 
-    public RefundRequestController(RefundRepository refundRepository) {
+    public RefundRequestController(RefundRepository refundRepository, TransactionRepository transactionRepository, int userID) {
         this.refundRepository = refundRepository;
+        this.transactionRepository = transactionRepository;
+        this.userID = userID;
+    }
+    public void createRefundRequest(int transactionID)
+    {
+        refundRepository.add(new RefundRequest(0,transactionID,RefundStatus.Pending));
+        System.out.println("refund request");
+    }
+    public List<Transaction> getUserTransaction(){
+        return transactionRepository.getTansactionByUserID(userID);
     }
 }
