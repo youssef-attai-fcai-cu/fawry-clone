23e0e7940dc51f6334a7eac2d4274eeb1b25809e
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-04 23:59:55 +0200

    Clean up

--- src/payment/CashOnDeliveryPayment.java
+++ src/payment/CashOnDeliveryPayment.java
@@ -3,6 +3,6 @@ package payment;
 public class CashOnDeliveryPayment implements PaymentMethod {
     @Override
     public void pay(float amount) {
-        System.out.println("cash on delivery strat");
+
     }
 }
