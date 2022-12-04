1e19a33fbef64324b1f02e53acc0d2d0d2ca416a
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-05 00:00:59 +0200

    Add concrete implementation of the refund repository

--- src/refund/InMemoryRefundRepository.java
+++ src/refund/InMemoryRefundRepository.java
@@ -4,26 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryRefundRepository implements RefundRepository {
-    private final List<RefundRequest> refundRequests=new ArrayList<>();
+    private final List<RefundRequest> refundRequests = new ArrayList<>();
 
 
     @Override
-    public void add(RefundRequest refundRequest) {
-        refundRequests.add(refundRequest);
+    public void add(int transactionID, int userID) {
+        refundRequests.add(new RefundRequest(this.refundRequests.size(), transactionID, userID, RefundStatus.Pending));
     }
-    public List<RefundRequest> gettAllRefundRequests(){
+
+    public List<RefundRequest> getAllRefundRequests() {
         return refundRequests;
     }
 
     @Override
     public void setRefundRequestStatus(int requestID, RefundStatus refundStatus) {
-        for (RefundRequest refundRequest: refundRequests) {
-            if(refundRequest.requestID==requestID)
-            {
+        for (RefundRequest refundRequest : refundRequests) {
+            if (refundRequest.requestID == requestID) {
                 refundRequest.setRefundStatus(refundStatus);
+//                TODO: Return money to user wallet
                 break;
             }
-
         }
+    }
+
+    @Override
+    public List<RefundRequest> getRefundRequestsByUserID(int userID) {
+        return this.refundRequests.stream().filter(refundRequest -> refundRequest.userID == userID).toList();
+    }
+
+    @Override
+    public int count() {
+        return this.refundRequests.size();
     }
 }
