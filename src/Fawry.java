3c0f60e124ebc0523b09d629b0103d5b503f4c3a
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-04 23:59:45 +0200

    Integrate controllers, views, and repositories

--- src/Fawry.java
+++ src/Fawry.java
@@ -2,19 +2,27 @@ import auth.*;
 import discount.DiscountRepository;
 import discount.InMemoryDiscountRepository;
 import payment.*;
+import refund.*;
 import search.SearchServicesController;
 import search.SearchServicesView;
-import service.ServiceProvider;
-import service.VodafoneRecharge;
+import service.*;
 import transactions.InMemoryTransactionRepository;
+import transactions.Transaction;
 import transactions.TransactionRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Fawry {
     private User currentUser = null;
     private final Scanner scanner = new Scanner(System.in);
+    private final TransactionRepository transactionRepository = new InMemoryTransactionRepository();
+    private final DiscountRepository discountRepository = new InMemoryDiscountRepository();
+    private final UserRepository userRepository = new InMemoryUserRepository();
+    private final RefundRepository refundRepository = new InMemoryRefundRepository();
+
 
     public static void main(String[] args) {
         Fawry fawry = new Fawry();
@@ -38,22 +46,21 @@ public class Fawry {
 //
 //        PaymentController paymentController = new PaymentController(new VodafoneRecharge(), transactionRepository, discountRepository, currentUser.id());
 //
-//        RefundRepository refundRepository = new InMemoryRefundRepository();
 
     }
 
     private void adminMenu() {
         while (this.currentUser != null) {
-            System.out.println("1. Add discounts\n" +
+            System.out.println("1. Manage discounts\n" +
                     "2. Manage refund requests\n" +
                     "3. Logout\n");
             int choice = inputChoice(1, 3);
             switch (choice) {
                 case 1 -> {
-                    System.out.println("Add discounts".toUpperCase());
+                    this.manageDiscounts();
                 }
                 case 2 -> {
-                    System.out.println("Manage refund requests".toUpperCase());
+                    this.manageRefundRequests();
                 }
                 case 3 -> {
                     this.logout();
@@ -65,8 +72,48 @@ public class Fawry {
         }
     }
 
+    private void manageRefundRequests() {
+        RefundResponseController refundResponseController = new RefundResponseController(refundRepository);
+        RefundResponseView refundResponseView = new RefundResponseView(refundResponseController);
+        refundResponseView.show();
+
+        System.out.println("Select a refund request by its ID");
+        int requestID = inputChoice(0, refundRepository.count());
+
+        System.out.println("Request state: " +
+                "1. Accept" +
+                "2. Reject" +
+                "3. Keep pending"
+        );
+        RefundStatus refundStatus;
+        switch (inputChoice(0, refundRepository.count())) {
+            case 1 -> {
+                refundStatus = RefundStatus.Accepted;
+            }
+            case 2 -> {
+                refundStatus = RefundStatus.Rejected;
+            }
+            default -> {
+                refundStatus = RefundStatus.Pending;
+            }
+        }
+
+        refundResponseView.setRefundStatus(requestID, refundStatus);
+    }
+
+    private void manageDiscounts() {
+        System.out.println("\nAdd a discount on:\n");
+        for (int i = 0; i < getAllServices().size(); i++) {
+            System.out.println((i + 1) + ". " + getAllServices().get(i).getServiceName());
+        }
+        System.out.println("0. All services");
+        int choice = inputChoice(0, getAllServices().size() - 1);
+
+//        TODO: Check if user chose 0, then add new overall discount rather than specific discount
+        discountRepository.addNewSpecificDiscount(inputChoice(1, 99), getAllServices().get(choice).getServiceName());
+    }
+
     public void auth() {
-        UserRepository userRepository = new InMemoryUserRepository();
         UserAuthController userAuthController = new UserAuthController(userRepository);
         UserSignInView userSignInView = new UserSignInView(userAuthController);
         UserSignUpView userSignUpView = new UserSignUpView(userAuthController);
@@ -92,27 +139,54 @@ public class Fawry {
         while (this.currentUser != null) {
             System.out.println("1. Search for services\n" +
                     "2. Make a refund request\n" +
-                    "3. Add funds to wallet\n" +
-                    "4. Logout\n");
+                    "3. See my refund requests\n" +
+                    "4. Add funds to wallet\n" +
+                    "5. Logout\n");
             int choice = inputChoice(1, 4);
             switch (choice) {
                 case 1 -> {
-//                    System.out.println("Search for services".toUpperCase());
                     this.searchForServices();
                 }
                 case 2 -> {
-                    System.out.println("Make a refund request".toUpperCase());
+                    this.requestRefund();
                 }
                 case 3 -> {
-                    System.out.println("Add funds to wallet".toUpperCase());
+                    this.myRequestRequests();
                 }
                 case 4 -> {
+                    this.addFundsToWallet();
+                }
+                case 5 -> {
                     this.logout();
                 }
                 default -> {
                     System.out.println("Invalid option");
                 }
             }
+        }
+    }
+
+    private void myRequestRequests() {
+        if (refundRepository.count() > 0) {
+            for (RefundRequest r : refundRepository.getRefundRequestsByUserID(currentUser.id())) {
+                System.out.println(r.toString());
+            }
+        } else {
+            System.out.println("\nYou have not requested any refunds\n");
+        }
+    }
+
+    private void addFundsToWallet() {
+
+    }
+
+    private void requestRefund() {
+        RefundRequestController refundRequestController = new RefundRequestController(refundRepository, transactionRepository, currentUser.id());
+        RefundRequestView refundRequestView = new RefundRequestView(refundRequestController);
+
+        if (refundRequestView.listTransactions(transactionRepository.getTransactionsByUserID(currentUser.id()))) {
+            int transactionID = inputChoice(0, transactionRepository.count());
+            refundRequestView.createRefundRequest(transactionID);
         }
     }
 
@@ -143,14 +217,12 @@ public class Fawry {
 
     private List<ServiceProvider> getAllServices() {
         return List.of(
-                new VodafoneRecharge()
+                new VodafoneRecharge(),
+                new We()
         );
     }
 
     private void payForService(ServiceProvider serviceProvider) {
-        TransactionRepository transactionRepository = new InMemoryTransactionRepository();
-        DiscountRepository discountRepository = new InMemoryDiscountRepository();
-
         PaymentController paymentController = new PaymentController(serviceProvider, transactionRepository, discountRepository, currentUser.id());
         PaymentView paymentView = new PaymentView(paymentController);
 
@@ -175,17 +247,24 @@ public class Fawry {
                 paymentMethod = new CreditCardPayment();
             }
         }
-
         paymentView.selectPaymentMethod(paymentMethod);
-        paymentView.fillPaymentForm();
+
+        Map<String, String> form = new HashMap<>();
+
+        for (PaymentFormField field : serviceProvider.getPaymentFormFields()) {
+            System.out.print(field.label() + ": ");
+            form.put(field.label(), this.scanner.nextLine());
+        }
+
+        paymentController.submitPaymentForm(form);
     }
 
     private int inputChoice(int start, int end) {
         int choice;
         do {
             System.out.print("\n> ");
-            choice = this.scanner.nextInt();
-        } while (end <= choice && choice >= start);
+            choice = Integer.parseInt(this.scanner.nextLine());
+        } while (end < choice && choice < start);
         return choice;
     }
 
