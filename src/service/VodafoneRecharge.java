56e3a3acb472c2fe5a176ece8c3a921af7c28761
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-04 23:56:15 +0200

    Add demo implementations of the ServiceProvider interface

--- src/service/VodafoneRecharge.java
+++ src/service/VodafoneRecharge.java
@@ -1,9 +1,23 @@
 package service;
 
-public class VodafoneRecharge implements ServiceProvider{
+import java.util.List;
+import java.util.Map;
+
+public class VodafoneRecharge implements ServiceProvider {
+    private final PaymentFormField mobileNumber = new TextField("Mobile number");
+    private final PaymentFormField amount = new TextField("Amount");
+
     @Override
-    public boolean handleForm() {
-        return false;
+    public List<PaymentFormField> getPaymentFormFields() {
+        return List.of(mobileNumber, amount);
+    }
+
+    @Override
+    public boolean handleForm(Map<String, String> form) {
+        String mobileNumber = form.get("Mobile number");
+        float amount = Float.parseFloat(form.get("Amount"));
+//        Use form fields to make the proper API request
+        return true;
     }
 
     @Override
@@ -12,12 +26,12 @@ public class VodafoneRecharge implements ServiceProvider{
     }
 
     @Override
-    public float getServiceBillAmount() {
-        return 0;
+    public String getServiceName() {
+        return "Vodafone recharge";
     }
 
     @Override
-    public String getServiceName() {
-        return "Vodafone recharge";
+    public float getBillAmount(Map<String, String> form) {
+        return Float.parseFloat(form.get("Amount"));
     }
 }
