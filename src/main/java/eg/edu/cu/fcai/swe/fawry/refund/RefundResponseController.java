d08f39bd15877a973be1af12ca5776828e1ece68
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-18 00:33:31 +0200

    Refactor

--- src/main/java/eg/edu/cu/fcai/swe/fawry/refund/RefundResponseController.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/refund/RefundResponseController.java
@@ -1,32 +1,58 @@
 package eg.edu.cu.fcai.swe.fawry.refund;
 
+import eg.edu.cu.fcai.swe.fawry.common.MissingFieldException;
+import eg.edu.cu.fcai.swe.fawry.common.ResourceNotFound;
+import eg.edu.cu.fcai.swe.fawry.common.Validator;
+import eg.edu.cu.fcai.swe.fawry.transaction.InMemoryTransactionRepository;
+import eg.edu.cu.fcai.swe.fawry.transaction.Transaction;
+import eg.edu.cu.fcai.swe.fawry.transaction.TransactionRepository;
+import eg.edu.cu.fcai.swe.fawry.wallet.InMemoryWalletRepository;
+import eg.edu.cu.fcai.swe.fawry.wallet.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/refundResponse")
 public class RefundResponseController {
     private final RefundRepository refundRepository;
+    private final WalletRepository walletRepository;
+    private final TransactionRepository transactionRepository;
 
     @Autowired
-    public RefundResponseController(RefundRepository refundRepository) {
-        this.refundRepository = refundRepository;
+    public RefundResponseController(
+            InMemoryRefundRepository _refundRepository,
+            InMemoryWalletRepository _walletRepository,
+            InMemoryTransactionRepository _transactionRepository
+    ) {
+        refundRepository = _refundRepository;
+        walletRepository = _walletRepository;
+        transactionRepository = _transactionRepository;
     }
 
-    @GetMapping
-    public List<RefundRequest> getAllRefundRequests() {
-        return refundRepository.getAllRefundRequests();
-    }
+    @PostMapping
+    public RefundStatus setRequestStatus(@RequestBody RefundResponseForm form) {
+        if (Validator.fieldDoesNotExist(form.requestId()))
+            throw new MissingFieldException("requestId");
 
-    @PutMapping
-    public RefundStatus setRefundRequestStatus(RefundResponseForm form) {
-//        TODO: Allow admin to set the refund request status
-        refundRepository.setRefundRequestStatus(form.requestID(), form.refundStatus());
+        if (Objects.isNull(form.refundStatus()))
+            throw new MissingFieldException("refundStatus");
+
+        RefundRequest refundRequest = refundRepository.setStatus(form.requestId(), form.refundStatus());
+
+        if (Objects.isNull(refundRequest))
+            throw new ResourceNotFound("Refund request", form.requestId());
+
+        Transaction transaction = transactionRepository.getById(refundRequest.transactionId());
+
+        if (Objects.isNull(transaction))
+            throw new ResourceNotFound("Transaction", refundRequest.transactionId());
+
+        walletRepository.updateUserBalance(refundRequest.userId(), transaction.amount());
         return form.refundStatus();
     }
 }
--- src/main/java/eg/edu/cu/fcai/swe/fawry/wallet/WalletController.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/wallet/WalletController.java
@@ -1,6 +1,12 @@
 package eg.edu.cu.fcai.swe.fawry.wallet;
 
-import eg.edu.cu.fcai.swe.fawry.auth.UserRepository;
+import eg.edu.cu.fcai.swe.fawry.common.InvalidAmount;
+import eg.edu.cu.fcai.swe.fawry.common.MissingFieldException;
+import eg.edu.cu.fcai.swe.fawry.common.ResourceNotFound;
+import eg.edu.cu.fcai.swe.fawry.common.Validator;
+import eg.edu.cu.fcai.swe.fawry.transaction.InMemoryTransactionRepository;
+import eg.edu.cu.fcai.swe.fawry.transaction.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/wallet")
 public class WalletController {
-    private final UserRepository userRepository;
+    private final WalletRepository walletRepository;
+    private final TransactionRepository transactionRepository;
 
-    public WalletController(UserRepository repository) {
-        userRepository = repository;
+    @Autowired
+    public WalletController(InMemoryWalletRepository _walletRepository, InMemoryTransactionRepository _transactionRepository) {
+        walletRepository = _walletRepository;
+        transactionRepository = _transactionRepository;
     }
 
     @PostMapping
-    public void addWalletBalance(@RequestBody AddWalletBalanceForm form) {
-        userRepository.updateWalletBalance(form.uid(), form.funds());
+    public Float addWalletBalance(@RequestBody AddWalletBalanceForm form) {
+        if (Validator.fieldDoesNotExist(form.userId()))
+            throw new MissingFieldException("userId");
+
+        if (form.amount() == null)
+            throw new MissingFieldException("amount");
+
+        if (form.amount() <= 0.0f)
+            throw new InvalidAmount(form.amount());
+
+        Float newBalance = walletRepository.updateUserBalance(form.userId(), form.amount());
+
+        if (newBalance == null)
+            throw new ResourceNotFound("User", form.userId());
+
+        transactionRepository.create(form.userId(), form.amount(), "Add funds to wallet");
+        return newBalance;
     }
 }
