fd83f65111a1c457855eb82f1f52467821d6df2a
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-18 00:33:17 +0200

    Refactor

--- src/main/java/eg/edu/cu/fcai/swe/fawry/refund/RefundRequestController.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/refund/RefundRequestController.java
@@ -1,31 +1,49 @@
 package eg.edu.cu.fcai.swe.fawry.refund;
 
-import eg.edu.cu.fcai.swe.fawry.transactions.InMemoryTransactionRepository;
-import eg.edu.cu.fcai.swe.fawry.transactions.Transaction;
+import eg.edu.cu.fcai.swe.fawry.common.MissingFieldException;
+import eg.edu.cu.fcai.swe.fawry.common.ResourceNotFound;
+import eg.edu.cu.fcai.swe.fawry.common.Validator;
+import eg.edu.cu.fcai.swe.fawry.transaction.Transaction;
+import eg.edu.cu.fcai.swe.fawry.transaction.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+
 
 @RestController
 @RequestMapping("/refundRequest")
 public class RefundRequestController {
-    public final InMemoryRefundRepository refundRepository;
-    public final InMemoryTransactionRepository transactionRepository;
+    public final RefundRepository refundRepository;
+    public final TransactionRepository transactionRepository;
 
     @Autowired
-    public RefundRequestController(InMemoryRefundRepository refundRepository, InMemoryTransactionRepository transactionRepository) {
-        this.refundRepository = refundRepository;
-        this.transactionRepository = transactionRepository;
+    public RefundRequestController(InMemoryRefundRepository _refundRepository, TransactionRepository _transactionRepository) {
+        refundRepository = _refundRepository;
+        transactionRepository = _transactionRepository;
+    }
+
+    @GetMapping
+    public List<RefundRequest> getAllRefundRequests() {
+        return refundRepository.getAll();
     }
 
     @PostMapping
-    public void createRefundRequest(@RequestBody RefundRequestForm form) {
-        refundRepository.add(form.transactionID(), form.uid());
-    }
+    public RefundRequest createRefundRequest(@RequestBody RefundRequestForm form) {
+        if (Validator.fieldDoesNotExist(form.transactionId()))
+            throw new MissingFieldException("transactionId");
 
-    @GetMapping("/{uid}")
-    public List<Transaction> getAllUserTransactions(@PathVariable Integer uid) {
-        return transactionRepository.getTransactionsByUserID(uid);
+        Transaction transaction = transactionRepository.getById(form.transactionId());
+
+        if (Objects.isNull(transaction))
+            throw new ResourceNotFound("Transaction", form.transactionId());
+
+        RefundRequest newRefundRequest = refundRepository.create(form.transactionId(), transaction.userId());
+
+        if (Objects.isNull(newRefundRequest))
+            throw new RefundAlreadyRequested(transaction.transactionId());
+
+        return newRefundRequest;
     }
 }
