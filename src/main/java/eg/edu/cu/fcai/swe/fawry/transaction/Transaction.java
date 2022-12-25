b0af6f02c9cbb7783fff88d2b21f391bd57d1fc2
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:33:46 +0200

    Refactor

--- src/main/java/eg/edu/cu/fcai/swe/fawry/transaction/Transaction.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/transaction/Transaction.java
@@ -1,4 +1,4 @@
 package eg.edu.cu.fcai.swe.fawry.transaction;
 
-public record Transaction(String transactionId, String userId, Float amount, String service) {
+public record Transaction(String transactionId, String userId, Float amount, String providerId) {
 }
