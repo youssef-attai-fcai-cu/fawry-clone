93b23b07b89ebe582ac532a3590e17e9e2f15efd
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:29:58 +0200

    Hide discount endpoints behind authentication

--- src/main/java/eg/edu/cu/fcai/swe/fawry/discount/DiscountController.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/discount/DiscountController.java
@@ -1,26 +1,49 @@
 package eg.edu.cu.fcai.swe.fawry.discount;
 
-import eg.edu.cu.fcai.swe.fawry.common.MissingFieldException;
+import eg.edu.cu.fcai.swe.fawry.auth.InMemoryUserRepository;
+import eg.edu.cu.fcai.swe.fawry.auth.UserRepository;
+import eg.edu.cu.fcai.swe.fawry.common.Response;
 import eg.edu.cu.fcai.swe.fawry.common.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/discount")
 public class DiscountController {
     private final DiscountRepository discountRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public DiscountController(DiscountRepository discountRepository) {
-        this.discountRepository = discountRepository;
+    public DiscountController(
+            InMemoryDiscountRepository _discountRepository,
+            InMemoryUserRepository _userRepository
+    ) {
+        discountRepository = _discountRepository;
+        userRepository = _userRepository;
+    }
+
+    @PostMapping
+    public Response addNewOverallDiscount(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody DiscountForm form) {
+        Validator.validateAdminToken(userRepository, token);
+        Validator.assertFieldExists("percentage", form.percentage());
+        discountRepository.addOverall(form.percentage());
+        return new Response(form.percentage() + "% discount was added to all services");
     }
 
     @GetMapping
     public List<Integer> getOverall() {
         return discountRepository.getAllOverall();
+    }
+
+    @PostMapping("/{providerId}")
+    public Response addNewSpecificDiscount(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @PathVariable String providerId, @RequestBody DiscountForm form) {
+        Validator.validateAdminToken(userRepository, token);
+        Validator.assertFieldExists("percentage", form.percentage());
+        discountRepository.addSpecific(providerId, form.percentage());
+        return new Response(form.percentage() + "% discount was added to providerId with ID: " + providerId);
     }
 
     @GetMapping("/{providerId}")
