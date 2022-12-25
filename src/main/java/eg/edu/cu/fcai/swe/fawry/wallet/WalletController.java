78702b2ec813863f39a727a8f205a79096042706
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:28:17 +0200

    Hide wallet endpoints behind authentication

--- src/main/java/eg/edu/cu/fcai/swe/fawry/wallet/WalletController.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/wallet/WalletController.java
@@ -1,25 +1,30 @@
 package eg.edu.cu.fcai.swe.fawry.wallet;
 
-import eg.edu.cu.fcai.swe.fawry.common.InvalidAmount;
-import eg.edu.cu.fcai.swe.fawry.common.MissingFieldException;
-import eg.edu.cu.fcai.swe.fawry.common.ResourceNotFound;
+import eg.edu.cu.fcai.swe.fawry.auth.InMemoryUserRepository;
+import eg.edu.cu.fcai.swe.fawry.auth.User;
+import eg.edu.cu.fcai.swe.fawry.auth.UserRepository;
+import eg.edu.cu.fcai.swe.fawry.common.Response;
 import eg.edu.cu.fcai.swe.fawry.common.Validator;
 import eg.edu.cu.fcai.swe.fawry.transaction.InMemoryTransactionRepository;
 import eg.edu.cu.fcai.swe.fawry.transaction.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wallet")
 public class WalletController {
+    private final UserRepository userRepository;
     private final WalletRepository walletRepository;
     private final TransactionRepository transactionRepository;
 
     @Autowired
-    public WalletController(InMemoryWalletRepository _walletRepository, InMemoryTransactionRepository _transactionRepository) {
+    public WalletController(
+            InMemoryUserRepository _userRepository,
+            InMemoryWalletRepository _walletRepository,
+            InMemoryTransactionRepository _transactionRepository
+    ) {
+        userRepository = _userRepository;
         walletRepository = _walletRepository;
         transactionRepository = _transactionRepository;
     }
@@ -31,22 +36,15 @@ public class WalletController {
     }
 
     @PostMapping
-    public Float addWalletBalance(@RequestBody AddWalletBalanceForm form) {
-        if (Validator.fieldDoesNotExist(form.userId()))
-            throw new MissingFieldException("userId");
+    public Response addFunds(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody AddFundsForm form) {
+        User user = Validator.validateUserToken(userRepository, token);
 
-        if (form.amount() == null)
-            throw new MissingFieldException("amount");
+        Validator.assertFieldExists("amount", form.amount());
+        Validator.assertNumberWithinRange("amount", form.amount(), 0.0f, Float.MAX_VALUE);
 
-        if (form.amount() <= 0.0f)
-            throw new InvalidAmount(form.amount());
+        float newBalance = walletRepository.updateUserBalance(user.userId(), form.amount());
 
-        Float newBalance = walletRepository.updateUserBalance(form.userId(), form.amount());
-
-        if (newBalance == null)
-            throw new ResourceNotFound("User", form.userId());
-
-        transactionRepository.create(form.userId(), form.amount(), "Add funds to wallet");
-        return newBalance;
+        transactionRepository.create(user.userId(), form.amount(), "Add funds to wallet");
+        return new Response("Added $" + form.amount() + " to your wallet. Your balance is now: $" + newBalance);
     }
 }
