2a1ad5cf53f62da73f87fba7c5f29cba63d0ff7f
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:37:29 +0200

    Refactor

--- src/main/java/eg/edu/cu/fcai/swe/fawry/common/InvalidAmount.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/common/InvalidAmount.java
@@ -1,10 +1,9 @@
 package eg.edu.cu.fcai.swe.fawry.common;
 
-import eg.edu.cu.fcai.swe.fawry.common.APIException;
 import org.springframework.http.HttpStatus;
 
 public class InvalidAmount extends APIException {
-    public InvalidAmount(Float amount) {
-        super("Invalid amount of funds: " + amount, HttpStatus.BAD_REQUEST);
+    public InvalidAmount(String what, Number amount) {
+        super("Invalid amount of " + what + ": " + amount, HttpStatus.BAD_REQUEST);
     }
 }
