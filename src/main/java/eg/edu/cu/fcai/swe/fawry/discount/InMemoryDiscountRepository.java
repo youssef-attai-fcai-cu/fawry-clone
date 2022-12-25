2de2f793e6b8beb434cca7e6319c52b892990e0a
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:31:04 +0200

    Simplify the get discounts operation

--- src/main/java/eg/edu/cu/fcai/swe/fawry/discount/InMemoryDiscountRepository.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/discount/InMemoryDiscountRepository.java
@@ -20,8 +20,8 @@ public class InMemoryDiscountRepository implements DiscountRepository {
     }
 
     @Override
-    public List<Discount> getAllOverall() {
-        return getAllSpecific("*");
+    public List<Integer> getAllOverall() {
+        return getAllSpecific("*").stream().map(Discount::percentage).toList();
     }
 
     @Override
