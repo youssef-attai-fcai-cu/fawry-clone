4a9eee4cb49fc1d1d5969a675d80882ee17d52a4
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:43:52 +0200

    Create wallet payment with the required repositories

--- src/main/java/eg/edu/cu/fcai/swe/fawry/payment/PaymentMethodFactory.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/payment/PaymentMethodFactory.java
@@ -1,9 +1,19 @@
 package eg.edu.cu.fcai.swe.fawry.payment;
 
+import eg.edu.cu.fcai.swe.fawry.wallet.WalletRepository;
+
 public class PaymentMethodFactory {
+    private final WalletRepository walletRepository;
+    private final String userId;
+
+    public PaymentMethodFactory(WalletRepository _walletRepository, String _userId) {
+        walletRepository = _walletRepository;
+        userId = _userId;
+    }
+
     public PaymentMethod create(PaymentOption method) {
         return switch (method) {
-            case WALLET -> new WalletPayment();
+            case WALLET -> new WalletPayment(walletRepository, userId);
             case CASH -> new CashOnDeliveryPayment();
             default -> new CreditCardPayment();
         };
