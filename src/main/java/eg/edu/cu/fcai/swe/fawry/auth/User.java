676e7ec6022096dfaef7cd7b8c37f90c3cbaa966
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:35:21 +0200

    Delegate isAdmin to AuthController

--- src/main/java/eg/edu/cu/fcai/swe/fawry/auth/User.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/auth/User.java
@@ -2,5 +2,8 @@ package eg.edu.cu.fcai.swe.fawry.auth;
 
 
 public record User
-        (String userId, Boolean isAdmin, String username, String email, String password) {
+        (String userId,
+         String username,
+         String email, String password,
+         String token) {
 }
