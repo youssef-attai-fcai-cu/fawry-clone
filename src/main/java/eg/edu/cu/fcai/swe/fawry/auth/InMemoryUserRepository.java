97d98ef20a956ccc8bd3bf43d431b306109d702a
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-25 18:36:20 +0200

    Separate users and admins into two different collections

--- src/main/java/eg/edu/cu/fcai/swe/fawry/auth/InMemoryUserRepository.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/auth/InMemoryUserRepository.java
@@ -4,38 +4,77 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class InMemoryUserRepository implements UserRepository {
     //List for Users
     private final List<User> users = new ArrayList<>();
+    private final List<User> admins = new ArrayList<>();
 
     // Adding new users in List
     @Override
-    public User create(boolean isAdmin, String username, String email, String password) {
-        User newUser = new User(isAdmin, username, email, password, "USER-" + UUID.randomUUID());
+    public User createUser(String username, String email, String password) {
+        User newUser = new User(
+                UserRepositoryUtils.newUserId(),
+                username,
+                email,
+                password,
+                UserRepositoryUtils.newUserToken()
+        );
         users.add(newUser);
         return newUser;
     }
 
     @Override
-    public User getEmailAndPassword(String email, String password) {
+    public User createAdmin(String username, String email, String password) {
+        User newAdmin = new User(
+                UserRepositoryUtils.newAdminId(),
+                username,
+                email,
+                password,
+                UserRepositoryUtils.newUserToken()
+        );
+        admins.add(newAdmin);
+        return newAdmin;
+    }
+
+    @Override
+    public User getUserByEmailAndPassword(String email, String password) {
         return users.stream().filter(user -> user.email().equals(email) && user.password().equals(password)).findFirst().orElse(null);
     }
 
     @Override
-    public User getByEmail(String email) {
+    public User getAdminByEmailAndPassword(String email, String password) {
+        return admins.stream().filter(admin -> admin.email().equals(email) && admin.password().equals(password)).findFirst().orElse(null);
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
         return users.stream().filter(user -> user.email().equals(email)).findFirst().orElse(null);
     }
 
     @Override
-    public User getByUsername(String username) {
+    public User getAdminByEmail(String email) {
+        return admins.stream().filter(admin -> admin.email().equals(email)).findFirst().orElse(null);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
         return users.stream().filter(user -> user.username().equals(username)).findFirst().orElse(null);
     }
 
     @Override
-    public User getById(String userId) {
-        return users.stream().filter(user -> user.id().equals(userId)).findFirst().orElse(null);
+    public User getAdminByUsername(String username) {
+        return admins.stream().filter(admin -> admin.username().equals(username)).findFirst().orElse(null);
+    }
+
+    @Override
+    public User getUserByToken(String token) {
+        return users.stream().filter(user -> user.token().equals(token)).findFirst().orElse(null);
+    }
+
+    @Override
+    public User getAdminByToken(String token) {
+        return admins.stream().filter(admin -> admin.token().equals(token)).findFirst().orElse(null);
     }
 }
--- src/main/java/eg/edu/cu/fcai/swe/fawry/auth/UserRepository.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/auth/UserRepository.java
@@ -2,13 +2,23 @@ package eg.edu.cu.fcai.swe.fawry.auth;
 
 public interface UserRepository {
 
-    User create(boolean isAdmin, String username, String email, String password);
+    User createUser(String username, String email, String password);
 
-    User getEmailAndPassword(String email, String password);
+    User createAdmin(String username, String email, String password);
 
-    User getByEmail(String email);
+    User getUserByEmailAndPassword(String email, String password);
 
-    User getByUsername(String username);
+    User getAdminByEmailAndPassword(String email, String password);
 
-    User getById(String userId);
+    User getUserByEmail(String email);
+
+    User getAdminByEmail(String email);
+
+    User getUserByUsername(String username);
+
+    User getAdminByUsername(String username);
+
+    User getUserByToken(String token);
+
+    User getAdminByToken(String token);
 }
