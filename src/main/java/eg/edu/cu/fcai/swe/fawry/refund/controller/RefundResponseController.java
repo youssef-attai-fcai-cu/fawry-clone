c07ccea4e5a94f3f0da4f34c07348f01499a7820
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-27 12:41:55 +0200

    Fix imports

--- src/main/java/eg/edu/cu/fcai/swe/fawry/refund/controller/RefundResponseController.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/refund/controller/RefundResponseController.java
@@ -8,7 +8,7 @@ import eg.edu.cu.fcai.swe.fawry.common.Response;
 import eg.edu.cu.fcai.swe.fawry.common.Validator;
 import eg.edu.cu.fcai.swe.fawry.refund.repository.InMemoryRefundRepository;
 import eg.edu.cu.fcai.swe.fawry.refund.model.RefundRequest;
-import eg.edu.cu.fcai.swe.fawry.refund.model.RefundStatus;
+import eg.edu.cu.fcai.swe.fawry.refund.RefundStatus;
 import eg.edu.cu.fcai.swe.fawry.refund.request.RefundResponseRequestBody;
 import eg.edu.cu.fcai.swe.fawry.transaction.repository.InMemoryTransactionRepository;
 import eg.edu.cu.fcai.swe.fawry.transaction.model.Transaction;
