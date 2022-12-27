0f0a782af6981585dea9e73160ed4e62f12aa2f3
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-27 12:41:39 +0200

    Fix imports

--- src/main/java/eg/edu/cu/fcai/swe/fawry/refund/model/RefundRequest.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/refund/model/RefundRequest.java
@@ -1,4 +1,6 @@
 package eg.edu.cu.fcai.swe.fawry.refund.model;
 
+import eg.edu.cu.fcai.swe.fawry.refund.RefundStatus;
+
 public record RefundRequest(String transactionId, RefundStatus refundStatus) {
 }
