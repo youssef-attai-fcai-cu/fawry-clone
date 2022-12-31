000aa7c91e28ce883b610aeca58bdc21aee323b3
youssefgalalnazem@gmail.com <youssefgalalnazem@gmail.com>
2023-01-01 00:14:03 +0200

    Add more info in token validation error message

--- src/main/java/eg/edu/cu/fcai/swe/fawry/common/exception/InvalidToken.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/common/exception/InvalidToken.java
@@ -3,7 +3,7 @@ package eg.edu.cu.fcai.swe.fawry.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class InvalidToken extends APIException {
-    public InvalidToken() {
-        super("Invalid token.", HttpStatus.BAD_REQUEST);
+    public InvalidToken(String who) {
+        super("Invalid " + who + " token.", HttpStatus.BAD_REQUEST);
     }
 }
