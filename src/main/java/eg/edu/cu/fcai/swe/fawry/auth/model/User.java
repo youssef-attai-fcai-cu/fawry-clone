94fb9c5404148046ad4fb1491c406c7f6f86ba52
youssefgalalnazem@gmail.com <youssefgalalnazem@gmail.com>
2023-01-01 00:14:08 +0200

    Reformat

--- src/main/java/eg/edu/cu/fcai/swe/fawry/auth/model/User.java
+++ src/main/java/eg/edu/cu/fcai/swe/fawry/auth/model/User.java
@@ -4,6 +4,7 @@ package eg.edu.cu.fcai.swe.fawry.auth.model;
 public record User
         (String userId,
          String username,
-         String email, String password,
+         String email,
+         String password,
          String token) {
 }
